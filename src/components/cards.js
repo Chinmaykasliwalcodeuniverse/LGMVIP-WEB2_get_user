ea0a74629ae246b862f2ce6335605c11f938faf1
Chinmaykasliwalcodeuniverse <96693041+Chinmaykasliwalcodeuniverse@users.noreply.github.com>
2022-02-13 13:09:26 +0530

    a

---
+++ src/components/cards.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Users = ({loading,users}) => {
+    return loading ? (   
+          <div id="main">
+  
+          </div>
+        ) : 
+        (
+          <div id="main">
+    
+          {users.map(user =>
+                      <div className="project column3">
+                        {
+                          console.log(user)
+                        }
+                        <div className="profile">
+                          <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+                          <h1 className="name">{user.first_name} {user.last_name}</h1>
+                          <p className="email">{user.email}</p>
+                          
+                        </div>
+                      </div>
+                    )
+          }
+        </div>
+        )
+}
+
+export default Users;
